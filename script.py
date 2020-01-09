b5ad04a5b7bbe59fb820c906953bbb1cfc659cf9
Christopher <chris@theantichris.com>
2020-01-09 09:38:00 -0500

    implemented linear search to find max value

--- script.py
+++ script.py
@@ -10,6 +10,15 @@ def linear_search(search_list, target_value):
 
     raise ValueError("{} not in list".format(target_value))
 
+def linear_search_largest(search_list):
+    maximum_score_index = None
+
+    for i in range(len(search_list)):
+        if maximum_score_index is None or search_list[i] > search_list[maximum_score_index]:
+            maximum_score_index = i
+
+    return maximum_score_index
+
 
 tour_locations = ["New York City", "Los Angeles", "Bangkok", "Istanbul", "London", "New York City", "Toronto"]
 target_city = "Knoxville"
